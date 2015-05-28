282f119b19936402fecccda7781d7ccf8d2921e0
Ryan Kinal <ryan.kinal@gmail.com>
2015-05-28 14:49:20 -0400

    Home page now has more of a 'roadmap' style

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,4 +2,48 @@
 
 angular.module('lessonsApp')
 	.controller('HomeCtrl', function($scope, $rootScope, $http, _, $location) {
+		$scope.categories = {
+			language: [
+				'data-types',
+				'control-structures',
+				'functions-scope',
+				'inheritance'
+			],
+			client: [
+				'client-modules',
+				'dom',
+				'events-listeners',
+				'client-side-storage',
+				'xhr',
+				'angular-apps',
+				'angular-routing',
+				'angular-services',
+				'angular-directives'
+			],
+			server: [
+				'node-npm',
+				'node-modules',
+				'file-access',
+				'database-access',
+				'http-routing',
+				'express',
+				'sequelize'
+			],
+			tools: [
+				'console',
+				'debugger',
+				'inspector',
+				'node-npm',
+				'angular-fullstack',
+				'bootstrap-css'
+			]
+		};
+
+		_.forEach($scope.categories, function(arr, index) {
+			$scope.categories[index] = _.map(arr, function(link) {
+				return _.filter($scope.lessons, function(lesson) {
+					return lesson.link === link;
+				})[0];
+			});
+		});
 	});
